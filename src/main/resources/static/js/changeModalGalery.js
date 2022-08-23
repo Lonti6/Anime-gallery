00f40e42c8ced8da98a9b127a80599092061b146
Lonti6 <72629247+Lonti6@users.noreply.github.com>
2022-08-23 23:06:12 +0500

    modal window

---
+++ src/main/resources/static/js/changeModalGalery.js
@@ -0,0 +1,13 @@
+let elements = document.getElementsByName("carrd");
+
+let modalLable = document.getElementById("cardTitle");
+let modalImg = document.getElementById("cardImg");
+let cardRating = document.getElementById("cardRating");
+
+for (let elem of elements) {
+    elem.onclick = function(){
+        modalLable.textContent = elem.querySelector('.cardTitle').textContent;
+        modalImg.src = elem.querySelector('.card-img-top').src;
+        cardRating.textContent = elem.querySelector('.cardRating').textContent;
+    }
+  }
